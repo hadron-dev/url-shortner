b43bf6cf1ac765ce63ff1a09f17540538cd573f8
rezafaraji <faraji.reza@outlook.com>
2024-07-05 09:21:34 -0400

    chore(shortener-service): Setup conventional commits plugin
    Adds plugin to configure to pre-commit hook that will enforce project commits adhere to the Conventional Commits standard

--- ktor-url-sortner/build.gradle.kts
+++ ktor-url-sortner/build.gradle.kts
@@ -4,6 +4,7 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "2.0.0"
     id("io.ktor.plugin") version "2.3.12"
+    id("it.nicolasfarabegoli.conventional-commits") version "3.1.3"
 }
 
 group = "dev.hadron"
@@ -18,6 +19,17 @@ application {
 
 repositories {
     mavenCentral()
+}
+
+// accepted types: fix, feat, build, chore, ci, docs, style, refactor, perf, test, tmp
+// simplified commit format: 'type(scope): message'
+// example commit: 'feat(android): Added app widget'
+// see link for more details on Conventional Commits
+// https://www.conventionalcommits.org/en/v1.0.0/
+conventionalCommits {
+    types += listOf("tmp")
+    scopes += listOf("shortener-service", "android", "ios")
+    failureMessage = "The commit message does not meet the Conventional Commit standard"
 }
 
 dependencies {
